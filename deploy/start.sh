85dd7ac46d470e81fa6a98f403e984cc74c4ab4b
Crt <crt@ibzoot.com>
2016-06-02 15:10:04 +0200

    fix deployment

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,4 +3,5 @@ cd /app/server/src/ServerTest2/
 sudo dotnet restore
 path=$(sudo dotnet publish --configuration Release | grep -Pio "(?<=published to )(.+)")
 cd $path
-sudo dotnet ServerTest2.dll &
+# Redirect stdout, stderr, and stdin to dev null to let aws continue deployment
+sudo dotnet ServerTest2.dll > /dev/null 2> /dev/null < /dev/null &
