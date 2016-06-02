8ebb10602ec39176ab59df3fd5a84755287080b8
Crt <crt@ibzoot.com>
2016-06-02 14:59:14 +0200

    update to run in bg

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,4 +3,4 @@ cd /app/server/src/ServerTest2/
 sudo dotnet restore
 path=$(sudo dotnet publish --configuration Release | grep -Pio "(?<=published to )(.+)")
 cd $path
-sudo dotnet ServerTest2.dll
+sudo dotnet ServerTest2.dll &
