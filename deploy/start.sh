10d748905fb2062d9fde1dd62c3b2950de31cbe8
Crt <crt@ibzoot.com>
2016-06-02 14:48:07 +0200

    sexier start

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /app/server/src/ServerTest2/
 sudo dotnet restore
-sudo dotnet run --configuration Release
+path=$(sudo dotnet publish --configuration Release | grep -Pio "(?<=published to )(.+)")
+cd $path
+sudo dotnet ServerTest2.dll
