33b382a546b585d127333ee256395f547c17c26b
Crt <crt@ibzoot.com>
2016-06-03 09:14:43 +0200

    no export

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -2,4 +2,3 @@
 mkdir -p /app/log
 	
 sudo rsync --delete-before --verbose --archive --exclude ".*" /app/serverTemp/ /app/server/ > /app/log/deploy.log
-export NETCORE_SERVER.URLS=http://unix:/app/server/kestrel.sock
